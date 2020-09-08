134d8f81c2b6e4a78f324596df2bb157c96ffe1b
L. Theodore Obonye <ltobonye@gmail.com>
2020-09-08 11:01:14 +0200

    fix

--- src/util/db.js
+++ src/util/db.js
@@ -2,14 +2,14 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 /** remote connection */
-let uri = 'mongodb+srv://team33H:T3@m33h2020@cluster0.dghpw.mongodb.net/test?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://team33H:T3@m33h2020@cluster0.dghpw.mongodb.net/test?retryWrites=true&w=majority';
 /** localhost */
 dotenv.config();
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-  uri = process.env.DB_PATH_DEV;
-} else {
-  uri = process.env.DB_PATH_PROD;
-}
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+//   uri = process.env.DB_PATH_DEV;
+// } else {
+//   uri = process.env.DB_PATH_PROD;
+// }
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const dbConnection = mongoose.connection;
