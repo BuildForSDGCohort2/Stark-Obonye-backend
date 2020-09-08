ecef5796f5d39400255e42c5387242d42b6ff7c5
L. Theodore Obonye <ltobonye@gmail.com>
2020-09-08 15:59:56 +0200

    fix

--- src/util/db.js
+++ src/util/db.js
@@ -11,20 +11,17 @@ dotenv.config();
 //   uri = process.env.DB_PATH_PROD;
 // }
 
-const db = 'mongodb+srv://team33H:T3@m33h2020@cluster0.dghpw.mongodb.net/sikaDB?retryWrites=true&w=majority';
-const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      db || process.env.DB_PATH_DEV,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    );
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error.message);
-    process.exit(1);
-  }
-};
-connectDB();
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb+srv://team33H:t3am33h2020@cluster0.dghpw.mongodb.net/sikaDB?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(async () => {
+  const collection = await client.db('test').collection('devices');
+  // perform actions on the collection object
+  // eslint-disable-next-line no-console
+  console.log(collection);
+  client.close();
+});
 
 const dbConnection = mongoose.connection;
 export default dbConnection;
