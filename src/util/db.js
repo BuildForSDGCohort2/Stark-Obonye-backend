c4b8f95302e12bb190aa93395d973e8c46919fd9
L. Theodore Obonye <ltobonye@gmail.com>
2020-09-08 08:14:27 +0200

    set up environment variable

--- src/util/db.js
+++ src/util/db.js
@@ -1,9 +1,17 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 /** remote connection */
 /* const uri = "mongodb+srv://team_275:04tLXMQ4mmllwy2P@cluster0-dghpw.mongodb.net/test?retryWrites=true&w=majority"; */
 /** localhost */
-const uri = 'mongodb://localhost:27017/sikaDB';
+dotenv.config();
+let uri = '';
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  uri = process.env.DB_PATH_DEV;
+} else {
+  uri = process.env.DB_PATH_PROD;
+}
+
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const dbConnection = mongoose.connection;
 export default dbConnection;
