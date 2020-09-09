ab61305a1e6080905fc4fe31a0af9cc036bf1517
L. Theodore Obonye <ltobonye@gmail.com>
2020-09-09 14:53:15 +0200

    ffix

--- src/util/db.js
+++ src/util/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = process.env.MONGODB_URI || 'mongodb://localhost:27017/sikaDB';
+const db = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/sikaDB';
 try {
   mongoose.connect(db, {
     useUnifiedTopology: true,
