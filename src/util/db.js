e1119772e1f1cb2dfbb4850ac19883bb0e6f4a9e
L. Theodore Obonye <ltobonye@gmail.com>
2020-09-09 16:03:17 +0200

    fix

--- src/util/db.js
+++ src/util/db.js
@@ -2,20 +2,48 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-dotenv.config();
+const { MongoClient } = require('mongodb');
+
 const PORT = process.env.MONGODB_PORT || 27017;
 const HOST = process.env.MONGODB_HOST || 'localhost';
+
+dotenv.config();
+
 const db = process.env.MONGODB_URI || `mongodb://${HOST}:${PORT}/sikaDB`;
-try {
-  mongoose.connect(db, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  }).then().catch((err) => console.log(err));
-  console.log('MongoDB is Connected...');
-} catch (err) {
-  console.error(err);
-  process.exit(1);
+// try {
+//   mongoose.connect(db, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+//   }).then().catch((err) => console.log(err));
+//   console.log('MongoDB is Connected...');
+// } catch (err) {
+//   console.error(err);
+//   process.exit(1);
+// }
+async function main() {
+  const client = new MongoClient(db, {
+    useUnifiedTopology: true
+  });
+  try {
+    await client.connect();
+    await client
+      .db()
+      .collection('exampleCollection')
+      .insertOne({
+        someKey: 'someVal'
+      });
+    const result = await client
+      .db()
+      .collection('exampleCollection')
+      .findOne({});
+    console.log(JSON.stringify(result));
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
 }
 
+main().catch(console.error);
 const dbConnection = mongoose.connection;
 export default dbConnection;
