25009925997be233c513094dd90fd76ad16b093a
L. Theodore Obonye <ltobonye@gmail.com>
2020-09-11 09:28:29 +0200

    fix

--- src/util/db.js
+++ src/util/db.js
@@ -10,40 +10,40 @@ const HOST = process.env.MONGODB_HOST || 'localhost';
 dotenv.config();
 
 const db = process.env.MONGODB_URI || `mongodb://${HOST}:${PORT}/sikaDB`;
-// try {
-//   mongoose.connect(db, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true
-//   }).then().catch((err) => console.log(err));
-//   console.log('MongoDB is Connected...');
-// } catch (err) {
-//   console.error(err);
-//   process.exit(1);
-// }
-async function main() {
-  const client = new MongoClient(db, {
-    useUnifiedTopology: true
-  });
-  try {
-    await client.connect();
-    await client
-      .db()
-      .collection('exampleCollection')
-      .insertOne({
-        someKey: 'someVal'
-      });
-    const result = await client
-      .db()
-      .collection('exampleCollection')
-      .findOne({});
-    console.log(JSON.stringify(result));
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
+try {
+  mongoose.connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  }).then().catch((err) => console.log(err));
+  console.log('MongoDB is Connected...');
+} catch (err) {
+  console.error(err);
+  process.exit(1);
 }
+// async function main() {
+//   const client = new MongoClient(db, {
+//     useUnifiedTopology: true
+//   });
+//   try {
+//     await client.connect();
+//     await client
+//       .db()
+//       .collection('exampleCollection')
+//       .insertOne({
+//         someKey: 'someVal'
+//       });
+//     const result = await client
+//       .db()
+//       .collection('exampleCollection')
+//       .findOne({});
+//     console.log(JSON.stringify(result));
+//   } catch (e) {
+//     console.error(e);
+//   } finally {
+//     await client.close();
+//   }
+// }
 
-main().catch(console.error);
+// main().catch(console.error);
 const dbConnection = mongoose.connection;
 export default dbConnection;
