1f11e544ffab6f57ca6436d5b75fae5e9d148ad2
L. Theodore Obonye <ltobonye@gmail.com>
2020-09-23 08:25:21 +0200

    fix

--- src/util/db.js
+++ src/util/db.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
 // const PORT = process.env.MONGODB_PORT || 27017;
 // const HOST = process.env.MONGODB_HOST || 'localhost';
@@ -10,35 +10,19 @@ const { MongoClient } = require('mongodb');
 dotenv.config();
 
 const db = process.env.MONGODB_URI;
-// mongoose
-//   .connect(db, {
-//     useUnifiedTopology: true,
-//     useNewUrlParser: true
-//   });
-async function main() {
-  const client = new MongoClient(db, {
-    useUnifiedTopology: true
-  });
-  try {
-    await client.connect();
-    await client
-      .db()
-      .collection('exampleCollection')
-      .insertOne({
-        someKey: 'someVal'
-      });
-    const result = await client
-      .db()
-      .collection('exampleCollection')
-      .findOne({});
-    console.log(JSON.stringify(result));
-  } catch (e) {
-    console.error(e);
-  } finally {
-    await client.close();
-  }
-}
 
-main().catch(console.error);
+try {
+  mongoose
+    .connect(db, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    })
+    .then()
+    .catch((err) => console.log(err));
+  console.log('MongoDB is Connected...');
+} catch (err) {
+  console.error(err);
+  process.exit(1);
+}
 const dbConnection = mongoose.connection;
 export default dbConnection;
